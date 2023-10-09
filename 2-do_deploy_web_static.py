b2be1a151535a4054e83f832ebec169fe484f665
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-10-09 23:12:24 +0200

    Task 3. Full deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,7 +18,7 @@ def do_deploy(archive_path):
     try:
         if not (path.exists(archive_path)):
             return False
-        put(archive_path, "/tmp/")
+        put(archive_path, '/tmp/')
         ts = archive_path[-18:-4]
         run('sudo mkdir -p /data/web_static/releases/\
             web_static_{}/'.format(ts))
