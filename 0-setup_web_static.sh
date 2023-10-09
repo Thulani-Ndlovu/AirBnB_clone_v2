a45c8f93f919d15def977ebf29cadabab464320e
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-10-09 22:00:14 +0200

    Task 0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -35,7 +35,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
 
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/
-	current/;\n\t}\n' /etc/nginx/sistes-available/default
+	current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/
 sites-enabled/default'
